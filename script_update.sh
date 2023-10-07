f55634b9e73cbbc802c159227785038ca68b356c
Joker <voix.momotalk@gmail.com>
2023-10-07 16:49:01 +0700

    Update docker-compose file

--- script_update.sh
+++ script_update.sh
@@ -1,11 +1,11 @@
 git pull origin main
 
-docker compose down
+# docker compose down
 
 docker volume prune
 
-docker rmi voixmomotalk/nestjs-thesis-api:1.0.0
+# docker rmi voixmomotalk/nestjs-thesis-api:1.0.0
 
-docker build -t voixmomotalk/nestjs-thesis-api:1.0.0 .
+# docker build -t voixmomotalk/nestjs-thesis-api:1.0.0 .
 
-docker compose up -d
+docker compose up -d --build

383dfb963fc5b299a276d0dd0382ae4a5d73feb1
Joker <voix.momotalk@gmail.com>
2023-10-10 02:23:19 +0700

    Add default status value

--- prisma/migrations/20231009181657_update/migration.sql
+++ prisma/migrations/20231009181657_update/migration.sql
@@ -1,2 +1,11 @@
 -- AlterTable
 ALTER TABLE "slots" ALTER COLUMN "status" SET DEFAULT 1;
+
+-- Insert a new SlotStatus record with a description
+INSERT INTO "slotStatuses" ("description") VALUES ('Not yet');
+
+-- Insert another SlotStatus record
+INSERT INTO "slotStatuses" ("description") VALUES ('Done');
+
+-- Insert another SlotStatus record
+INSERT INTO "slotStatuses" ("description") VALUES ('Cancel');
